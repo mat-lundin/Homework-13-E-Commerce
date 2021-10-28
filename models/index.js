6c4ed894083714a5eb25e63911c394e5d52fa44e
Mat Lundin <matmll12@gmail.com>
2021-10-28 17:21:52 -0500

    fixed belongsToMany in models index

--- models/index.js
+++ models/index.js
@@ -13,13 +13,13 @@ Category.hasMany(Product, {
   foreignKey: 'category_id'
 })
 // Products belongToMany Tags (through ProductTag)
-Product.belongToMany(Tag, {
+Product.belongsToMany(Tag, {
   through: ProductTag,
   foreignKey: 'product_id'
 })
 
 // Tags belongToMany Products (through ProductTag)
-Tag.belongToMany(ProductTag, {
+Tag.belongsToMany(ProductTag, {
   through: ProductTag,
   foreignKey: 'tag_id'
 })
